c7b7c754996666c3cf7ddf96b6f38bd7a7ed4ef6
h4llow3En <mail@felixdoering.com>
2018-07-06 19:38:52 +0200

    Update docs to newest version

---
+++ aliases.js
@@ -0,0 +1,2 @@
+var ALIASES = {};
+ALIASES['mac_notification_sys'] = {};
---
+++ implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["mac_notification_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"enum\" href=\"mac_notification_sys/error/enum.ApplicationError.html\" title=\"enum mac_notification_sys::error::ApplicationError\">ApplicationError</a>",synthetic:true,types:["mac_notification_sys::error::ApplicationError"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"enum\" href=\"mac_notification_sys/error/enum.NotificationError.html\" title=\"enum mac_notification_sys::error::NotificationError\">NotificationError</a>",synthetic:true,types:["mac_notification_sys::error::NotificationError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
