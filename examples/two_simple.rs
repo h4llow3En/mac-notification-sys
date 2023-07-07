ba226cd4ab0fcdfb756f3df992089bc3ecb7239d
Hendrik Sollich <hendrik@hoodie.de>
2023-07-07 15:18:14 +0200

    fix: do not crash when selecting the title of a drop down menu

---
+++ examples/two_simple.rs
@@ -0,0 +1,28 @@
+use mac_notification_sys::*;
+
+fn main() {
+    let bundle = get_bundle_identifier_or_default("firefox");
+    println!("{}", bundle);
+
+    set_application(&bundle).unwrap();
+
+    Notification::default()
+        .title("Danger")
+        .subtitle("Will Robinson")
+        .message("Run away as fast as you can")
+        .send()
+        .unwrap();
+
+    Notification::default()
+        .title("two")
+        .message("two")
+        .send()
+        .unwrap();
+
+    Notification::default()
+        .title("NOW")
+        .message("Without subtitle")
+        .sound("Submarine")
+        .send()
+        .unwrap();
+}
