48440a3fbd4e94294bf9045f974153d92b7e876e
zhangxu <zhangxu@knd.com.cn>
2024-10-12 11:51:03 +0800

    fix: wrong type of build flag parameter

--- build.rs
+++ build.rs
@@ -21,7 +21,7 @@ fn main() {
             // `cc` doesn't try to pick up on this automatically, but `clang` needs it to
             // generate a "correct" Objective-C symbol table which better matches XCode.
             // See https://github.com/h4llow3En/mac-notification-sys/issues/45.
-            .flag(format!("-mmacos-version-min={}", min_version))
+            .flag(&format!("-mmacos-version-min={}", min_version))
             .compile("notify");
 
         println!("cargo:rerun-if-env-changed={}", DEPLOYMENT_TARGET_VAR);
