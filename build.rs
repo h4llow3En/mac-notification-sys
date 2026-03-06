71be2a6fdb54287fadc50b4d38b6aade1d76536d
h4llow3En <development@felixdoering.com>
2026-03-06 11:11:39 +0100

    feat: Update Rust edition to 2024 and MSRV to minimum of dependencies

--- build.rs
+++ build.rs
@@ -8,24 +8,23 @@ fn main() {
             String::from(match var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
                 "x86_64" => "10.8",  // NSUserNotificationCenter first showed up here.
                 "aarch64" => "11.0", // Apple Silicon started here.
-                arch => panic!("unknown arch: {}", arch),
+                arch => panic!("unknown arch: {arch}"),
             })
         });
 
         cc::Build::new()
             .file("objc/notify.m")
-            // .flag("-fmodules")
             .flag("-Wno-deprecated-declarations")
             // `cc` doesn't try to pick up on this automatically, but `clang` needs it to
             // generate a "correct" Objective-C symbol table which better matches XCode.
             // See https://github.com/h4llow3En/mac-notification-sys/issues/45.
-            .flag(format!("-mmacos-version-min={}", min_version))
+            .flag(format!("-mmacos-version-min={min_version}"))
             .compile("notify");
 
         // Link the macOS frameworks that provide the missing symbols:
         println!("cargo:rustc-link-lib=framework=AppKit"); // for NSImage, other UI types
 
-        println!("cargo:rerun-if-env-changed={}", DEPLOYMENT_TARGET_VAR);
+        println!("cargo:rerun-if-env-changed={DEPLOYMENT_TARGET_VAR}");
         println!("cargo:rerun-if-changed=build.rs");
         println!("cargo:rerun-if-changed=objc");
     }
